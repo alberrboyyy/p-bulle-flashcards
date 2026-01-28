4c2a69d1fdb214edc3a4cbe8703bc314c28cd8bd
Albert Braimi <albertbraimi@gmail.com>
2026-01-28 16:02:06 +0100

    [1h][WOP] feat(step 8): finalisation

--- app-teachers-adonisjs/app/controllers/teachers_controller.ts
+++ app-teachers-adonisjs/app/controllers/teachers_controller.ts
@@ -1,3 +1,5 @@
+import { teacherValidator } from '#validators/teacher'
+import Section from '#models/section'
 import Teacher from '#models/teacher'
 import type { HttpContext } from '@adonisjs/core/http'
 import { dd } from '@adonisjs/core/services/dumper'
@@ -17,41 +19,53 @@ export default class TeachersController {
   /**
    * Display form to create a new record
    */
-  async create({}: HttpContext) {}
-  /**
-   * Handle form submission for the create action
-   */
-  async store({ request }: HttpContext) {}
-  /**
-   * Show individual record
-   */
+  async create({ view }: HttpContext) {
+    const sections = await Section.query().orderBy('name', 'asc')
+    return view.render('pages/teachers/create', { title: "Ajout d'un enseignant", sections })
+  }
+
+  async store({ request, response, session }: HttpContext) {
+    const payload = await request.validateUsing(teacherValidator)
+    await Teacher.create(payload)
+    session.flash('success', 'Enseignant ajouté avec succès !')
+    return response.redirect().toRoute('home')
+  }
+
   async show({ params, view }: HttpContext) {
     const teacher = await Teacher.query().where('id', params.id).preload('section').firstOrFail()
 
     return view.render('pages/teachers/show', { title: "Détail d'un enseignant", teacher })
   }
-  async edit({ params }: HttpContext) {}
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params, request }: HttpContext) {}
-  /**
-   * Delete record
-   */
-  async destroy({ params, session, response }: HttpContext) {
-    // 1. On cherche l'enseignant à supprimer
+
+  async edit({ params, view }: HttpContext) {
+    const teacher = await Teacher.findOrFail(params.id)
+    const sections = await Section.query().orderBy('name', 'asc')
+    return view.render('pages/teachers/edit', {
+      title: `Modifier l'enseignant ${teacher.firstname} ${teacher.lastname}`,
+      teacher,
+      sections,
+    })
+  }
+
+  async update({ params, request, response, session }: HttpContext) {
     const teacher = await Teacher.findOrFail(params.id)
 
-    // 2. On le supprime
+    const payload = await request.validateUsing(teacherValidator)
+    teacher.merge(payload)
+    await teacher.save()
+    session.flash('success', 'Enseignant modifié avec succès')
+    return response.redirect().toRoute('home')
+  }
+
+  async destroy({ params, session, response }: HttpContext) {
+    const teacher = await Teacher.findOrFail(params.id)
     await teacher.delete()
 
-    // 3. On prépare un message de succès (Flash message)
     session.flash(
       'success',
       `L'enseignant ${teacher.lastname} ${teacher.firstname} a été supprimé avec succès!`
     )
 
-    // 4. On redirige vers l'accueil
     return response.redirect().toRoute('home')
   }
 }
